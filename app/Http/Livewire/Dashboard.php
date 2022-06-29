a3701566bf2f8b46250cf5f67c7e5a42e03d80c4
Asad-101 <asad_101@gmail.com>
2022-06-29 23:07:18 +0500

    push

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -7,6 +7,7 @@ use App\Models\Bill;
 use App\Models\Site;
 use App\Models\HomeExpense;
 use App\Models\EmployeeExpense;
+use App\Models\Task;
 use carbon\Carbon;
 use Auth;
 
@@ -24,6 +25,7 @@ class Dashboard extends Component
     public $mTotalExpense;
     public $mPaid_amount;
     public $mUnpaid_amount;
+    public $tasksToday;
 
     public function mount(){
         $this->paid_amount = Bill::where('debit', '>' , '0')->sum('debit');
@@ -32,12 +34,16 @@ class Dashboard extends Component
         $this->employeeExpense = EmployeeExpense::sum('price');
         $this->homeExpense = HomeExpense::sum('price');
         $this->totalExpense = $this->homeExpense + $this->employeeExpense;
-        $this->blackListSites = Site::get();
+        $this->blackListSites = Site::where('credit', '>=', 'blacklist')->where('credit', '<', 'good')->get();
         $this->mEmployeeExpense = EmployeeExpense::whereMonth('created_at', Carbon::now()->month)->sum('price');
         $this->mHomeExpense = HomeExpense::whereMonth('created_at', Carbon::now()->month)->sum('price');
         $this->mTotalExpense = $this->mHomeExpense + $this->mEmployeeExpense;
         $this->mPaid_amount = Bill::where('debit', '>' , '0')->whereMonth('created_at', Carbon::now()->month)->sum('debit');
         $this->mUnpaid_amount = Bill::where('credit', '>' , '0')->whereMonth('created_at', Carbon::now()->month)->sum('credit');
+        $this->tasksToday = Task::whereDate('created_at', Carbon::today())->get();
+        $this->todayBills = Bill::whereDate('created_at', Carbon::today())->get();
+        $this->tasksMonthly = Task::whereMonth('created_at', Carbon::now()->month)->get();
+        $this->monthlyBills = Bill::whereMonth('created_at', Carbon::now()->month)->get();
         if(Auth::user()->group == "employee")
         {
             return redirect()->route("employee-task-unaccepted.index");
--- resources/views/components/page-title.blade.php
+++ resources/views/components/page-title.blade.php
@@ -1,7 +1,11 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="md:flex md:items-center md:justify-between bg-indigo-500 mb-2 py-5 px-2 border border-transparent rounded-md shadow-sm ">
     <div class="flex-1 min-w-0">
-        <h2 class="text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate">{{$title}}</h2>
+        <h2 {!! $attributes->merge([
+            'class' => 'text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate'
+            ]) !!}>
+            {{$title}}
+        </h2>
     </div>
     <div class="mt-4 flex md:mt-0 md:ml-4">
 {{--        <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">Edit</button>--}}
